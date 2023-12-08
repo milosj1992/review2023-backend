a5067981aeef7d774cd071ae1669267a3c356217
milosj1992 <84614678+milosj1992@users.noreply.github.com>
2023-12-08 20:55:34 +0100

    Update host.js

--- host.js
+++ host.js
@@ -1,23 +1,23 @@
 const express = require("express");
-// require("dotenv").config();
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
  const app = express();
  const port = process.env.PORT||8000; // You can change the port as needed
 
 // // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // // Routes
-// const authRoutes = require("./src/routes/auth");
-// const userRoutes = require("./src/routes/users");
-// const faqRoutes= require("./src/routes/faq");
-// // Use the defined routes
-// app.use("/", authRoutes);
-// app.use("/", userRoutes);
-// app.use("/", faqRoutes);
+const authRoutes = require("./src/routes/auth");
+const userRoutes = require("./src/routes/users");
+const faqRoutes= require("./src/routes/faq");
+// Use the defined routes
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+app.use("/", faqRoutes);
 
 
 // Start the server
